93320b68d2b6074e18fef8c206e69e4d520f64ab
yuxiaolong <yuxl@kongdao.me>
2019-08-27 11:35:55 +0800

    chore: add command

--- autoMount.sh
+++ autoMount.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # chkconfig:   2345 10 90 
-#
+# chkconfig --add autoMount.sh
+# chkconfig autoMount.sh on
 # description:  自动mount， 关闭swap 启动 kubelet
 mount -t vboxsf code  /var/code
 swapoff -a
